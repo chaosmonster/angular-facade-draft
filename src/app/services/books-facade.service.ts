0e7cb0050f56a0058d0a037f4d5ae42bbd918ecb
Martin Sonnenholzer <martin.sonnenholzer@googlemail.com>
2017-08-01 07:15:44 +0200

    "fixed" the multi subscriber do multiple onSubscribe problem (don't think this is a fix though. Also added a second subscriber to test it

--- src/app/services/books-facade.service.ts
+++ src/app/services/books-facade.service.ts
@@ -7,25 +7,36 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/defer';
 import 'rxjs/add/operator/let';
 import {BookDataService} from './book-data.service';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class BooksFacadeService {
 
-  public books$ = this.store.select(fromRoot.selectBooks).let((observable) => {
-    return Observable.defer(() => {
+  public books$ = this.store.select(fromRoot.selectBooks)
+    .do(() => {
+      console.log('do');
+    })
+    .let((observable) => {
+      // Sander pointed out that the initBooks function would be called every time a new subscriber is added.
+      // I found out that the let operator will only be called once? But this doesn't feel right.
+      console.log('let');
       this.initBooks();
+      // return Observable.defer(() => {
+      //   return observable;
+      // });
       return observable;
-    });
   });
 
   constructor(private store: Store<fromRoot.State>, private bookDataService: BookDataService) {
   }
 
   public addBook(book: Book) {
+    console.log('addBook');
     this.store.dispatch(new BookActions.AddBook(book));
   }
 
   public initBooks() {
+    console.log('initBooks');
     this.bookDataService.getBooks().subscribe((books: Book[]) => {
       this.store.dispatch(new BookActions.AddBooks(books));
     });
